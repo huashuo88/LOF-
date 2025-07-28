4c987457612fae89c41c4c9d3322b7408e7260c4
huashuo88 <137618268+huashuo88@users.noreply.github.com>
2025-07-28 17:17:52 +0800

    Update lof.py

--- lof.py
+++ lof.py
@@ -25,7 +25,7 @@ class LOF:
         self.session = requests.Session()
         header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36",}
         self.session.headers.update(header)
-        self.urlBase = "https://www.jisilu.cn/data/lof/detail/"
+        self.urlBase = "https://www.jisilu.cn/data/lof/#stock"
         self.urlLOF = "https://www.jisilu.cn/data/lof/stock_lof_list/?___jsl=LST___t="
 
     def getInfo(self, id):
